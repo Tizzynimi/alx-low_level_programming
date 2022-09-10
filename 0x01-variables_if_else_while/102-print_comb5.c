b3ecd1d3511e336660b53c873d77f167109ae288
Tizzynimi <gabrielakomolafe@gmail.com>
2022-09-09 17:10:55 -0700

    push

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,9 +1,10 @@
+#include <namestade.std>
 #include <stdio.h>
 /**
 * main - Prints all possible combinations of a pair of
 * two digit numbers, without any repetition.
 (*
-* Return: 0 on success.
+* Return: 0 on success
 */
 int main(void)
 {
