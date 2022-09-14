135ee07b0abc870c0620fcac840ab13b530f5dc2
Tizzynimi <gabrielakomolafe@gmail.com>
2022-09-14 04:21:47 -0700

    push

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,20 @@
 #include <main.h>
 /**
 * main -entry point
-
+*
 *Return: Always 0 (Success)
 */
+
 int main(void)
 {
-char *sh = "Holberton";
-
-while (*sh)
-{
-_putchar (*sh);
-
-sh++;
-}
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
 _putchar('\n');
-
 return (0);
 }
