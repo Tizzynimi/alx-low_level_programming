caacc2143621f17faab7d011fde7c8773c5429f3
Tizzynimi <gabrielakomolafe@gmail.com>
2022-09-17 16:48:51 +0100

    push

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
  * print_alphabet - print Holberton
@@ -8,7 +8,7 @@
 
 void print_alphabet(void)
 {
-	int i;
+int i;
 
 	for (i = 'a'; i <= 'z'; i++)
 	{
