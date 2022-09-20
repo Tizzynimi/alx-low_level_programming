c32362f11bb764f5f14575c53ae4f9c7a0418083
Tizzynimi <gabrielakomolafe@gmail.com>
2022-09-20 13:00:17 +0100

    push

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,18 +1,19 @@
 #include "main.h"
+
 /**
- * _puts - function that prints a string
- * @str: first value to check
+ * _puts - a function that prints a string
+ * @str: string input
+ * Return: string
  *
  */
 
 void _puts(char *str)
 {
-	int i = 0;
+	int 1;
 
-	while (str[i] != '\0');
+	for (i = 0;str[i] != '\0'; i++)
 	{
 		_putchar(str[i]);
-		i++;
 	}
 	_putchar('\n');
 }
