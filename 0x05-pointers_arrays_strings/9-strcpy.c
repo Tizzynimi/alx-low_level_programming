44b14871320d7f8d59dce76a2439726e1168ce9a
Tizzynimi <gabrielakomolafe@gmail.com>
2022-09-20 14:18:39 +0100

    push

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -7,13 +7,18 @@
  * Return: string
  */
 
-char *_strcpy(char *dest, char *src)
-{
-	int i = 0;
+#define LEN 4
+void _memset(char *dest, char *src);
+char *_strcpy(char *dest, char *src);
 
-	for (; src[i] != '\0'; i++)
-	{
-		dest[i] = src[i];
-	}
-	return (dest);
+int main(void)
+	char cpy[LEN + 1] = {0};
+char *str;
+char *ret;
+
+_memset(cpy, 'H', LEN);
+str = "Z";
+ret = _strcpy(cpy, str);
+printf("%s\n%s\n%s\n", str, cpy, ret);
+return (0);
 }
