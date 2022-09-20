2dae0ce4902f90c2c558b4f08873daabf44009ed
Tizzynimi <gabrielakomolafe@gmail.com>
2022-09-20 12:41:15 +0100

    push

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-*  _strlen - function that returns the length of a string
+* _strlen - function that returns the length of a string
 * @s: char input
 * return: length of the input string
 */
