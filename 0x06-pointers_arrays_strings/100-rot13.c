bb55835ea7d39db48e5a8d3d12d88223d6e29786
Tizzynimi <gabrielakomolafe@gmail.com>
2022-09-22 06:38:47 -0700

    push

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -5,9 +5,26 @@
  * *rot13 - is a simple letter substitution that encodes letters
  *
  * @s: input action
- *
+ * Return: the point to dest
  */
 
-char *rot13(char *str)
+char *rot13(char *s)
 {
-int i = 
+int count = 0, i;
+char alphabeth[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+char rot13[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+
+while (*(s + count) != '\0')
+{
+	for (i = 0; i < 52; i++)
+	{
+		if (*(s + count) == alphabet[i])
+		{
+			*(s + count) = rot13[i];
+			break;
+		}
+	}
+	count++;
+}
+return (s);
+}
