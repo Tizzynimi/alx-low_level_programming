5c2c4ce6865b0c3b987ea44bde9931be77bfe0c7
Tizzynimi <gabrielakomolafe@gmail.com>
2022-09-22 06:15:04 -0700

    push

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * *rot13 - is a simple letter substitution that encodes letters
+ *
+ * @s: input action
+ *
+ */
+
+char *rot13(char *str)
+{
+int i = 
